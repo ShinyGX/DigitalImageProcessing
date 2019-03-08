df8519deda88f4bcc8cf241d689801c05810082f
Billy <591243801@qq.com>
2019-03-08 21:21:08 +0800

    exercise2

---
+++ Exercise2/main.cpp
@@ -0,0 +1,120 @@
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+typedef struct ImageData
+{
+	BYTE * img;
+	int length;
+	int width, height;
+}IMGDATA;
+
+typedef struct GrayHistogram
+{
+	float gray[256] = {0};
+	int pixelCount = 0;
+	void normalize();
+	void paint();
+
+private :
+	bool isNormalize = false;
+}GRAYHISTOGRAM;
+
+IMGDATA loadImage(const std::string& path);
+GRAYHISTOGRAM getHistogram(IMGDATA data);
+
+
+
+int main()
+{	
+	IMGDATA data = loadImage("bitmap/gray.bmp");
+	GRAYHISTOGRAM grayhistogram = getHistogram(data);
+	grayhistogram.normalize();
+	grayhistogram.paint();
+	system("pause");
+	return 0;
+}
+
+
+
+GRAYHISTOGRAM getHistogram(const IMGDATA data)
+{
+	GRAYHISTOGRAM grayhistogram;
+	int point = 0;
+	for (int i = 0; i < data.height; i++)
+	{
+		for(int j = 0;j < data.width;j++)
+		{
+			grayhistogram.gray[data.img[point++]]++;
+		}
+
+		while (point % 4 != 0)
+			point++;
+	}
+
+	grayhistogram.pixelCount = data.width * data.width;
+	return grayhistogram;
+}
+
+
+void GrayHistogram::normalize()
+{
+	for (float& i : gray)
+	{
+		i = i / pixelCount;
+	}
+
+	isNormalize = true;
+}
+
+void GrayHistogram::paint()
+{
+	for(int i = 0;i < 256;i++)
+	{
+		std::cout << i << " : ";
+		if(!isNormalize)
+			for (int j = 0; j < gray[i] / 100; j++)
+			{
+				std::cout << "*";
+			}
+		else
+			for (int j = 0; j < gray[i] * (static_cast<float>(pixelCount) / 100); j++)
+			{
+				std::cout << "*";
+			}
+
+		std::cout << std::endl;
+	}
+}
+
+
+
+IMGDATA loadImage(const std::string& path)
+{
+	std::ifstream ifstream;
+	ifstream.open(path, std::ios::binary);
+	if (!ifstream.is_open())
+		return {};
+
+	BITMAPFILEHEADER fileHeader;
+	BITMAPINFOHEADER infoHeader;
+	RGBQUAD rgbquad[256];
+
+	ifstream.read(reinterpret_cast<char*>(&fileHeader), sizeof(BITMAPFILEHEADER));
+	ifstream.read(reinterpret_cast<char *>(&infoHeader), sizeof(BITMAPINFOHEADER));
+	
+	ifstream.read(reinterpret_cast<char *>(&rgbquad), sizeof(RGBQUAD) * infoHeader.biClrUsed);
+
+	BYTE *img = new BYTE[infoHeader.biSizeImage];
+	ifstream.read(reinterpret_cast<char*>(img), infoHeader.biSizeImage);
+
+	IMGDATA imgdate;
+	imgdate.img = img;
+	imgdate.length = infoHeader.biSizeImage;
+	imgdate.width = infoHeader.biWidth;
+	imgdate.height = infoHeader.biHeight;
+
+	ifstream.close();
+	return imgdate;
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 数字图像处理练习
 
 [Exercise1 BMP图片处理](https://blog.csdn.net/a591243801/article/details/88291495)
+
+[Exercisel2 直方图处理]
